ce29a250ada9676daa5698f47cdfc11c2107e9cb
chensheng12330 <chensheng12330@gmail.com>
2019-10-18 20:41:31 +0800

    加入icloud云存储

---
+++ Home/Controllers/SHSettingViewController.h
@@ -0,0 +1,17 @@
+//
+//  SHSettingViewController.h
+//  INewsX
+//
+//  Created by sherwin.chen on 2019/10/16.
+//  Copyright © 2019 Sherwin.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SHSettingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Home/Controllers/SHSettingViewController.m
@@ -0,0 +1,32 @@
+//
+//  SHSettingViewController.m
+//  INewsX
+//
+//  Created by sherwin.chen on 2019/10/16.
+//  Copyright © 2019 Sherwin.Chen. All rights reserved.
+//
+
+#import "SHSettingViewController.h"
+
+@interface SHSettingViewController ()
+
+@end
+
+@implementation SHSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
